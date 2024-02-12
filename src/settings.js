e08ccabef4339053034ee0ffa54757367dd625bd
Chang Yeol <ckdduf138@naver.com>
2024-02-12 21:47:16 +0900

    FEAT: 빌드

--- src/settings.js
+++ src/settings.js
@@ -1,1 +1,1 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Game.fire",orientation:"",jsList:[]};
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Main.fire",orientation:"",jsList:[]};
